928d2d9ff54d895be56591dec06af9306221bf58
Maria-Nascimento06 <nascimentoeduarda182@gmail.com>
2025-07-11 17:11:40 -0300

    Add files via upload

---
+++ Filme.java
@@ -0,0 +1,14 @@
+public class Filme {
+    public static void main(String[] args) {
+        System.out.printf("Esse é o Screen Match");
+        System.out.println("Filme: Titanic: James Cameron");
+        int anoDeLancamento = 2012;
+        System.out.println("Ano de lançamento:" + anoDeLancamento);
+        System.out.println("O filme narra a história do famoso naufrágio do Titanic, que ocorreu em 15 de abril de 1912, e entrelaça a ficção com eventos históricos. \nA trama centraliza-se no romance entre Jack Dawson, um artista pobre interpretado por Leonardo DiCaprio, e Rose DeWitt Bukater, \numa jovem de classe alta, vivida por Kate Winslet. \nO filme explora temas como amor, sacrifício e as divisões sociais da época.");
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 4.6;
+
+        double media = (4.5 + 4.5 + 5.0) / 3;
+        System.out.println(media);
+    }
+}
