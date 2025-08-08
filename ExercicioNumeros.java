44260a43bfbf0081b1c45c50863b7ba7d58a31cf
Maria-Nascimento06 <nascimentoeduarda182@gmail.com>
2025-08-08 17:09:12 -0300

    Add files via upload

---
+++ ExercicioNumeros.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ExercicioNumeros {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Informe um número");
+        var number = scanner.nextInt();
+        var keepVerify = true;
+        while (keepVerify){
+
+            System.out.println("Informe o número para verificação");
+            var toVerify  = scanner.nextInt();
+            if (toVerify < number) {
+                System.out.printf("Informe um número maior que %s\n", number);
+                continue;
+            }
+
+            var result = toVerify % number;
+            keepVerify = result == 0;
+            System.out.printf("%s %% %s = %s \n", toVerify, number, result);
+            if (result != 0) break;
+
+            }
+    }
+
+}
---
+++ Repeticao.java
@@ -0,0 +1,14 @@
+public class Repeticao {
+
+
+    public static void main(String[] args) {
+        for (var i = 1; i <= 100; i++) {
+            if (i == 100) ;
+
+            System.out.println(i);
+            }
+        }
+    }
+
+
+
